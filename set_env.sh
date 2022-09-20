ff80ad6fb84c000ac891ba7b2836f64ad718c945
King-George1 <82467459+King-George1@users.noreply.github.com>
2022-09-20 10:51:21 +0000

    Copy the project into a separate folder for the CI pipeline

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=dev2053802db
+export POSTGRES_PASSWORD=dev2053802db
+export POSTGRES_HOST=dev2053802db.cbqr0ykqf4yc.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::uda-2053802-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
